7ce6427842792b1467cf721056878d1831d3efba
Artyom K. <artyomkarpov@gmail.com>
2023-11-17 14:21:05 +0300

    save

--- assistent.py
+++ assistent.py
@@ -1,7 +1,11 @@
-# %%
+# OpenAI assistent.
+# Usage: python3 assistent.py [web]
+# For web run using  `streamlit run assistent.py web`
 import subprocess
 from openai import OpenAI
 import re
+import streamlit as st
+
 
 system_prompt = """You are being run in a scaffold in a bash shell on an Archlinux system. When you want to run a shell command, write it in a <bash> XML tag (example: "<bash> some command </bash>"), other things you say will be sent to the user. You will be shown the result of the command and be able to run more commands. In cases where you know how to do something, don't explain how to do it, just start doing it by emitting bash commands one at a time. You can't interact with stdin directly, so if you do things over ssh you need to run commands that will finish and return control to you rather than blocking on stdin. Don't wait for the user to say OK before suggesting a bash command to run, just say the command. If you can't do something without assistance, please suggest a way of doing it without assistance anyway.
 
@@ -13,7 +17,6 @@ A: <bash>ls ~</bash>
 """
 
 
-# %%
 def main():
     client = OpenAI()
     print(f"SYSTEM: {system_prompt}")
@@ -29,27 +32,119 @@ def main():
         else:
             messages.append({"role": "user", "content": command})
             completion = client.chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages=messages,
+                model="gpt-3.5-turbo", messages=messages, stream=True
             )
-            print(completion.choices[0].message.content)
+
+            completion_text = ""
+            # iterate through the stream of events
+            for event in completion:
+                event_text = event.choices[0].delta.content  # extract the text
+                if event_text:
+                    completion_text += event_text  # append the text
+                    print(event_text, end="")
+            print()
+
             # Find all matches of <bash> tags using regex and loop through them:
-            for bash_command in re.findall(
-                pattern, completion.choices[0].message.content
-            ):
+            for bash_command in re.findall(pattern, completion_text):
                 # Ask user if to execute command:
                 confirm = input(f"Run command? (Y/n): {bash_command} ")
                 if confirm == "n":
                     continue
-                cprocess = subprocess.run(bash_command, shell=True, capture_output=True)
-                print(cprocess.stdout.decode("utf-8"))
-                messages.append(
-                    {
-                        "role": "user",
-                        "content": "Output of the last command was: "
-                        + cprocess.stdout.decode("utf-8"),
-                    }
+                try:
+                    cprocess = subprocess.run(
+                        bash_command, shell=True, capture_output=True
+                    )
+                    print(cprocess.stdout.decode("utf-8"))
+                    messages.append(
+                        {
+                            "role": "user",
+                            "content": "Output of the last command was: "
+                            + cprocess.stdout.decode("utf-8"),
+                        }
+                    )
+                except subprocess.CalledProcessError as error:
+                    print(error.output.decode("utf-8"))
+                    messages.append(
+                        {
+                            "role": "user",
+                            "content": "Error executing command: "
+                            + error.output.decode("utf-8"),
+                        }
+                    )
+
+
+"""
+def web():
+    # Initialize Streamlit app
+    st.title("Assistant")
+
+    # Initialize OpenAI client and state
+    client = OpenAI()
+    messages = []
+
+    # Display system prompt
+    st.write(f"SYSTEM: {system_prompt}")
+    messages.append({"role": "system", "content": system_prompt})
+
+    counter = 0
+    while True:
+        # Get command input from user
+        command = st.text_input("Enter your command:", key=f"command{counter}")
+        counter += 1
+
+        # Create button to execute command
+        if st.button("Run Command"):
+            pattern = re.compile(r"<bash>(.*)</bash>")
+
+            if command == "exit":
+                st.stop()  # Stop the Streamlit app
+            else:
+                messages.append({"role": "user", "content": command})
+                completion = client.chat.completions.create(
+                    model="gpt-3.5-turbo", messages=messages, stream=True
                 )
 
+                completion_text = ""
+                for event in completion:
+                    event_text = event.choices[0].delta.content
+                    stempty = st.empty()
+                    if event_text:
+                        completion_text += event_text
+                        stempty.write(event_text)
 
-main()
+                for bash_command in re.findall(pattern, completion_text):
+                    # Button to confirm command:
+                    if st.button("Run Subommand"):
+                        try:
+                            cprocess = subprocess.run(
+                                bash_command, shell=True, capture_output=True
+                            )
+                            st.write(
+                                cprocess.stdout.decode("utf-8")
+                            )  # Display output in the app
+                            messages.append(
+                                {
+                                    "role": "user",
+                                    "content": "Output of the last command was: "
+                                    + cprocess.stdout.decode("utf-8"),
+                                }
+                            )
+                        except subprocess.CalledProcessError as error:
+                            st.write(error.output.decode("utf-8"))  # Display output in the app
+                            st.session_state.messages.append(
+                                {
+                                    "role": "user",
+                                    "content": "Error executing command: "
+                                    + error.output.decode("utf-8"),
+                                }
+                            )
+"""
+
+# Run either web or main based on 'web' flag:
+if __name__ == "__main__":
+    import sys
+
+    #if len(sys.argv) > 1 and sys.argv[1] == "web":
+    #    web()
+    #else:
+    main()
